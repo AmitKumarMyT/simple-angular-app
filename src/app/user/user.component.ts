9290f2699d648a6d75cd3642c782fa4bd51c9b0d
nV3nom <amitkumarmaity11@gmail.com>
2024-07-17 12:49:10 +0530

    implemented dynamic name changes

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,9 +1,13 @@
 import { Component } from '@angular/core';
+import { DUMMY_USERS } from '../dummy-users';
 
+const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
 @Component({
   selector: 'app-user',
   standalone: true,
   templateUrl: './user.component.html',
   styleUrl: './user.component.css'
 })
-export class UserComponent {}
+export class UserComponent {
+  selectedUser = DUMMY_USERS[randomIndex] // will be made available to the template
+}
