e598f11727bc94e1f2d00aa21337bf9b5aa18ace
nV3nom <amitkumarmaity11@gmail.com>
2024-07-17 14:08:13 +0530

    chore: Update user component to require avatar and name inputs

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -10,8 +10,8 @@ import { Component, Input} from '@angular/core';
 })
 export class UserComponent {
 
-  @Input() avatar!: string;
-  @Input() name!: string;
+  @Input({required:true}) avatar!: string;
+  @Input({required:true}) name!: string;
 
   get imagePath(){
     return 'assets/users/' + this.avatar;
