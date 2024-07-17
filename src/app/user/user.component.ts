6e0c61dcaa3fe1cf64d9f3425dd1ac3ea98d458a
nV3nom <amitkumarmaity11@gmail.com>
2024-07-17 14:05:49 +0530

    chore: Refactor user component to use input properties for avatar and name

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,7 +1,7 @@
-import { Component, computed, signal } from '@angular/core';
-import { DUMMY_USERS } from '../dummy-users';
+import { Component, Input} from '@angular/core';
 
-const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
+
+
 @Component({
   selector: 'app-user',
   standalone: true,
@@ -9,18 +9,15 @@ const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
   styleUrl: './user.component.css'
 })
 export class UserComponent {
-  selectedUser = signal(DUMMY_USERS[randomIndex]); // will be made available to the template
 
-  imagePath = computed(() => 'assets/users/' + this.selectedUser().avatar);
+  @Input() avatar!: string;
+  @Input() name!: string;
 
-  //uses zone.js to detect changes
-  // get imagePath(){
-  //   return 'assets/users/' + this.selectedUser.avatar
-  // }
-
+  get imagePath(){
+    return 'assets/users/' + this.avatar;
+  }
   onSelectUser(){
-    const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
-    this.selectedUser.set(DUMMY_USERS[randomIndex]);
+
   }
 }
 
