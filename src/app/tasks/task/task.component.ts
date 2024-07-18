b6585bfdbd02704043965b3c0af0c792cedd7773
nV3nom <amitkumarmaity11@gmail.com>
2024-07-18 17:08:38 +0530

    Update task component to display task details dynamically

--- src/app/tasks/task/task.component.ts
+++ src/app/tasks/task/task.component.ts
@@ -1,4 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+
+interface Task{
+  id: string;
+  userId: string;
+  title: string;
+  summary: string;
+  dueDate: string;
+
+}
 
 @Component({
   selector: 'app-task',
@@ -8,5 +17,5 @@ import { Component } from '@angular/core';
   styleUrl: './task.component.css'
 })
 export class TaskComponent {
-
+  @Input({required:true}) task!: Task;
 }
