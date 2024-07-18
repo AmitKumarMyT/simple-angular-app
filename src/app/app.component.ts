f28fd82f478d6771013ea698855a24da320e99c7
nV3nom <amitkumarmaity11@gmail.com>
2024-07-18 12:01:41 +0530

    chore: Update app component to display tasks for selected user

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,18 +3,24 @@ import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
 import { UserComponent } from "./user/user.component";
 import { DUMMY_USERS } from './dummy-users';
+import { TasksComponent } from './tasks/tasks.component';
+
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, UserComponent],
+  imports: [RouterOutlet, HeaderComponent, UserComponent, TasksComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'first-angular-app';
   users = DUMMY_USERS;
+  selectedUserId = 'u1';
+  get selectedUser(){
+    return this.users.find((user) => user.id === this.selectedUserId);
+  }
 
   onSelectUser(id: string){
-    console.log('Selected user with id: ' + id);
+    this.selectedUserId = id;
   }
 }
