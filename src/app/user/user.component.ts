6ce38271968044accab2be16fe67da85fe80e1f1
nV3nom <amitkumarmaity11@gmail.com>
2024-07-18 11:45:36 +0530

    chore: Update user component to require id, avatar, and name inputs

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,5 +1,4 @@
-import { Component, Input} from '@angular/core';
-
+import { Component, Input, Output, EventEmitter} from '@angular/core';
 
 
 @Component({
@@ -9,15 +8,16 @@ import { Component, Input} from '@angular/core';
   styleUrl: './user.component.css'
 })
 export class UserComponent {
-
-  @Input({required:true}) avatar!: string;
-  @Input({required:true}) name!: string;
+   @Input({required:true}) id!: string;
+   @Input({required:true}) avatar!: string;
+   @Input({required:true}) name!: string;
+   @Output() select = new EventEmitter<string>();
 
   get imagePath(){
     return 'assets/users/' + this.avatar;
   }
   onSelectUser(){
-
+    this.select.emit(this.id);
   }
 }
 
